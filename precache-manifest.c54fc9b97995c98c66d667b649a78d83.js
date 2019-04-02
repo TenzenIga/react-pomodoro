854aa0a5799eb9e780ac4a9af79dddad2008ed1c
TenzenIga <arhan-93@list.ru>
2019-04-03 02:41:57 +0300

    Updates

---
+++ precache-manifest.c54fc9b97995c98c66d667b649a78d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54d79b643e0062695daebe3ab04dc286",
+    "url": "/TenzenIga/react-pomodoro/static/media/tuturu_1.54d79b64.mp3"
+  },
+  {
+    "revision": "26a8bcbd19558634989b",
+    "url": "/TenzenIga/react-pomodoro/static/js/runtime~main.4c0a897d.js"
+  },
+  {
+    "revision": "8773ac0c7b5bd327a896",
+    "url": "/TenzenIga/react-pomodoro/static/js/main.cd4ebf95.chunk.js"
+  },
+  {
+    "revision": "fe1bda3a86e46f0635ee",
+    "url": "/TenzenIga/react-pomodoro/static/js/2.1698cdb7.chunk.js"
+  },
+  {
+    "revision": "8773ac0c7b5bd327a896",
+    "url": "/TenzenIga/react-pomodoro/static/css/main.e5e1caaf.chunk.css"
+  },
+  {
+    "revision": "fe1bda3a86e46f0635ee",
+    "url": "/TenzenIga/react-pomodoro/static/css/2.634acf5a.chunk.css"
+  },
+  {
+    "revision": "4545f3221114d7c620e86db03c93f00d",
+    "url": "/TenzenIga/react-pomodoro/index.html"
+  }
+];
